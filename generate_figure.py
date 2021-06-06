253fd23a7d007bc245b6d285bd5a96c7180403ed
Peter <peter@armnova>
2021-06-06 15:42:57 -0400

    reorg

--- generate_figure.py
+++ generate_figure.py
@@ -1,13 +1,14 @@
+import argparse
+import pathlib
 import pickle
-import time
 
-import matplotlib.pyplot as plt
 import hjson
+import matplotlib.pyplot as plt
 import numpy as np
 import osmnx as ox
 
 
-def main():
+def generate():
     ox.config(log_console=False)
 
     with open("places.hjson", 'r') as f:
@@ -16,23 +17,26 @@ def main():
     gdf = ox.geocode_to_gdf(list(places.values()))
     print(gdf.display_name)
 
-    with open("saved_data.pkl", 'rb') as f:
-        data = pickle.load(f)
-
     for place in places.keys():
-        if place in data:
+        graph_filename = pathlib.Path(f'{place.replace(" ", "-")}.pkl')
+        if graph_filename.exists():
             continue
+
         print(f"Generate data for {place}")
 
-        # get undirected graphs with edge bearing attributes
-        graph = ox.graph_from_place(place, network_type="drive")
-        graph_undirected = ox.add_edge_bearings(ox.get_undirected(graph))
-        data[place] = {
-            'name':  place,
-            'graph': graph_undirected,
+        graph_directed = ox.graph_from_place(place, network_type="drive")
+        graph_undirected = ox.add_edge_bearings(ox.get_undirected(graph_directed))
+        bin_counts, bin_edges = ox.bearing._bearings_distribution(graph_undirected, 36, 1)
+        entropy = ox.bearing.orientation_entropy(graph_undirected)
+
+        data = {
+            'name':       place,
+            'bin_counts': bin_counts,
+            'bin_edges':  bin_edges,
+            'entropy':    entropy,
         }
 
-        with open(f"saved_data.pkl", 'wb') as f:
+        with open(graph_filename, 'wb') as f:
             pickle.dump(data, f)
 
 
@@ -66,5 +70,13 @@ def plot():
 
 
 if __name__ == '__main__':
-    main()
-    # plot()
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    generate_parser = subparsers.add_parser('generate')
+    generate_parser.set_defaults(func=generate)
+    plot_parser = subparsers.add_parser('plot')
+    plot_parser.set_defaults(func=plot)
+
+    args = parser.parse_args()
+    args.func(args)
